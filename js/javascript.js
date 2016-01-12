50d0e2007d3b5be7e449dd6064e4a949883ae507
Alex Larson and Wade Grubbs <wadegrubbs@gmail.com>
2016-01-12 11:19:27 -0800

    got madlib working

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,25 @@
 jQuery(document).ready(function() {
 
-  $("button#hello").click(function() {
-    $("ul#left").prepend("<li>Hello</li>");
-  });
+$("form").submit(function(event) {
+  var person1Input = $("input#person1").val();
+  $("span.person1").text(person1Input);
 
-  $("button#goodbye").click(function() {
-    $("ul#left").prepend("<li>Goodbye</li>");
-  });
+  var person2Input = $("input#person2").val();
+  $("span.person2").text(person2Input);
+  //
+  var animalInput = $("input#animal").val();
+  $("span.animal").text(animalInput);
+  //
+  var exclamationInput = $("input#exclamation").val();
+  $("span.exclamation").text(exclamationInput);
 
-  $("button#stop").click(function() {
-    $("ul#right").prepend("<li>Stop!</li>");
-    $("ul#right").children("li").first().click(function() {
-      alert('hi');
-    });
+  var verbInput = $("input#verb").val();
+  $("span.verb").text(verbInput);
 
-  });
+  var nounInput = $("input#noun").val();
+  $("span.noun").text(nounInput);
 
+  event.preventDefault();
+})
 
-
-
-  })
+})
