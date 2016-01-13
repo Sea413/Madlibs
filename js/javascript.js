cb8057224f1ee04ba7edac92517dc89e2032f2d6
Alex Larson and Sean Peerenboom <seanmp42@gmail.com>
2016-01-13 13:44:45 -0800

    added forEach loop to madlib

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,33 @@
 jQuery(document).ready(function() {
 
 $("form").submit(function(event) {
-  var person1Input = $("input#person1").val();
-  $("span.person1").text(person1Input);
+var values = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-  var person2Input = $("input#person2").val();
-  $("span.person2").text(person2Input);
+values.forEach(function(squid) {
+
+  var newSquid = $('#' + squid).val();
+
+  $('.' + squid).text(newSquid);
+
+});
+
+  // var person1Input = $("input#person1").val();
+  // $("span.person1").text(person1Input);
   //
-  var animalInput = $("input#animal").val();
-  $("span.animal").text(animalInput);
+  // var person2Input = $("input#person2").val();
+  // $("span.person2").text(person2Input);
+  // //
+  // var animalInput = $("input#animal").val();
+  // $("span.animal").text(animalInput);
+  // //
+  // var exclamationInput = $("input#exclamation").val();
+  // $("span.exclamation").text(exclamationInput);
   //
-  var exclamationInput = $("input#exclamation").val();
-  $("span.exclamation").text(exclamationInput);
-
-  var verbInput = $("input#verb").val();
-  $("span.verb").text(verbInput);
-
-  var nounInput = $("input#noun").val();
-  $("span.noun").text(nounInput);
+  // var verbInput = $("input#verb").val();
+  // $("span.verb").text(verbInput);
+  //
+  // var nounInput = $("input#noun").val();
+  // $("span.noun").text(nounInput);
 
   event.preventDefault();
 })
